1c8640aff200402d315688ebf07e45c0161e7659
Shymaxer <yushkevuch1337@gmail.com>
2018-09-20 21:44:01 +0300

    no message

--- src/index.js
+++ src/index.js
@@ -1,30 +1,50 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.array=[];
+    this.comparator= (a, b) => a - b;
   }
 
   add(element) {
     // your implementation
+    this.array.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.array[index];
   }
 
   get length() {
     // your implementation
+    return this.array.length;
   }
 
   toArray() {
     // your implementation
+    return this.array;
   }
 
   sort(indices) {
     // your implementation
+    indices.sort(this.comparator);
+
+let rep;
+
+      for (let i = 0; i < indices.length; i++){
+        for(let j = i; j < indices.length; j++){
+        if (this.comparator(this.array[indices[i]], this.array[indices[j]]) > 0) {
+            rep = this.array[indices[i]];
+            this.array[indices[i]] = this.array[indices[j]];
+            this.array[indices[j]] = rep;
+        }
+}
+}
   }
 
   setComparator(compareFunction) {
     // your implementation
+    this.comparator = compareFunction;
   }
 }
 
